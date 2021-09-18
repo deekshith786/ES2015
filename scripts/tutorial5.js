771f42220843924109e539cdd60d3bb152068bd1
Deekshith <76718957+deekshith786@users.noreply.github.com>
2021-09-18 18:11:24 +0530

    Funtional scope & hoisting

---
+++ scripts/tutorial5.js
@@ -0,0 +1,11 @@
+function greetPerson(name) {
+    if (name === 'deekshith') {
+        var greet = "hello deekshith";
+    }
+    else {
+        var greet = "hi there";
+    }
+    console.log(greet);
+}
+greetPerson("deekshith");
+//# sourceMappingURL=tutorial5.js.map
