7e1104970abfae6dfa29adb4d6e58c3de37958c0
Deekshith <76718957+deekshith786@users.noreply.github.com>
2021-09-18 18:27:54 +0530

    let keyword

--- scripts/tutorial5.js
+++ scripts/tutorial5.js
@@ -1,11 +1,32 @@
 function greetPerson(name) {
+    var greet;
     if (name === 'deekshith') {
-        var greet = "hello deekshith";
+        greet = "hello deekshith";
     }
     else {
-        var greet = "hi there";
+        greet = "hi there";
     }
+    //this doesnt work becasuse the let keyword is not hoisted
+    //var has funtional scope and let has block scope
     console.log(greet);
+    var x = 11;
+    var y = 22;
+    if (x == 11) {
+        var x = 111; // var keyword funtional scope
+        var y_1 = 222; // let keyword block scope 
+        console.log(x);
+        console.log(y_1);
+    }
+    console.log(x);
+    console.log(y);
+    // let varible cannot be used before declatarion because it doesnt support hoisting
+    // let varible cannot be re-declared but it can be in differnt block
+    // is valid
+    var a = 10;
+    var a = 20;
+    // not valid
+    var p = 30;
+    // let p = 40;
 }
 greetPerson("deekshith");
 //# sourceMappingURL=tutorial5.js.map
--- tutorial5.ts
+++ tutorial5.ts
@@ -1,11 +1,38 @@
 function greetPerson(name){
+    
+    let greet;
     if(name === 'deekshith'){
-         var greet = "hello deekshith";
+        greet = "hello deekshith";
     } else{
-        var greet = "hi there";
+       greet = "hi there";
     }
+    //this doesnt work becasuse the let keyword is not hoisted
+    //var has funtional scope and let has block scope
     console.log(greet);
     
+
+    var x = 11
+    var y = 22
+    if(x == 11)
+    {
+        var x = 111; // var keyword funtional scope
+        let y = 222; // let keyword block scope 
+        console.log(x);
+        console.log(y);        
+    }
+    console.log(x);
+    console.log(y);
+
+    // let varible cannot be used before declatarion because it doesnt support hoisting
+    // let varible cannot be re-declared but it can be in differnt block
+
+    // is valid
+    var a = 10
+    var a = 20
+
+    // not valid
+    let p = 30;
+    // let p = 40;
 }
 
 greetPerson("deekshith");
