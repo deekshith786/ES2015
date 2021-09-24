003fd6c375e17d052c0994aacfa9f238bf88f6e3
Deekshith <76718957+deekshith786@users.noreply.github.com>
2021-09-24 15:11:43 +0530

    let vs const

---
+++ scripts/tutorial9.js
@@ -0,0 +1,10 @@
+// let vs const 
+// use let when new re-assignment is expected  
+// use const when new value assignment is not expected 
+var PI = 3.14;
+var maxSize = 100;
+var a = 5, b = 10;
+a = a + b;
+b = a - b;
+a = a - b;
+//# sourceMappingURL=tutorial9.js.map
---
+++ tutorial9.ts
@@ -0,0 +1,11 @@
+// let vs const 
+// use let when new re-assignment is expected  
+// use const when new value assignment is not expected 
+
+const PI = 3.14;
+const maxSize = 100;
+
+let a= 5, b= 10;
+a = a+b;
+b = a-b;
+a = a-b; 
