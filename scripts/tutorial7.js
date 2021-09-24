5ba9def788b145f46fff9e487496f1e00bd40558
Deekshith <76718957+deekshith786@users.noreply.github.com>
2021-09-24 15:06:51 +0530

    const keyword

--- scripts/tutorial7.js
+++ scripts/tutorial7.js
@@ -4,4 +4,18 @@ var _loop_1 = function (i) {
 for (var i = 0; i < 5; i++) {
     _loop_1(i);
 }
+// let keyword is block scope and not hoisted
+// const keyword is also block scope and not hoisted as well
+// const keyword cannot be changed once assigned and they must be initialized when declared
+var number1;
+var number2 = 15;
+// object with const keyword
+var obj1 = {
+    name: "deekshith"
+};
+console.log(obj1.name);
+// this cannot be done as its a constant
+// obj1 = {}
+// this is possible, we cannot assign the new object to the obj1 because of const but can the value can be changed 
+obj1.name = "dixit";
 //# sourceMappingURL=tutorial7.js.map
--- tutorial7.ts
+++ tutorial7.ts
@@ -1,2 +1,25 @@
 for( let i=0; i<5;i++)
     setTimeout(function(){console.log(i);}, 1000)
+
+// let keyword is block scope and not hoisted
+
+// const keyword is also block scope and not hoisted as well
+// const keyword cannot be changed once assigned and they must be initialized when declared
+
+let number1;
+const number2 = 15;
+
+// object with const keyword
+const obj1 = {
+      name:"deekshith"
+};
+console.log(obj1.name);
+
+// this cannot be done as its a constant
+// obj1 = {}
+
+// this is possible, we cannot assign the new object to the obj1 because of const but can the value can be changed 
+obj1.name = "dixit"
+
+
+ 
