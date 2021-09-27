9a3b96f1aa3715297c7b479267f45c48a4038b5c
Deekshith <76718957+deekshith786@users.noreply.github.com>
2021-09-27 10:44:26 +0530

    array in typescript

---
+++ scripts/tutorial23.js
@@ -0,0 +1,9 @@
+// Array declarations
+var stringArray = [];
+var myArray = [];
+var anyArr = [];
+// tuples in typescript
+var mytuple = ["hello", 10];
+console.log(mytuple[0]);
+console.log(mytuple[1]);
+//# sourceMappingURL=tutorial23.js.map
---
+++ tutorial23.ts
@@ -0,0 +1,11 @@
+// Array declarations
+let stringArray : string[] = [];
+let myArray: Array<string> = [];
+
+let anyArr: any[] = [];
+
+// tuples in typescript
+let mytuple: [string, number] = ["hello", 10];
+console.log(mytuple[0]);
+console.log(mytuple[1]);
+
