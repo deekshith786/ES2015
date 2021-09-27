2b9da56a79c586418806decbecb83b5a388d5788
Deekshith <76718957+deekshith786@users.noreply.github.com>
2021-09-27 10:06:48 +0530

    declarations and annotations (type inferance in typescript)

---
+++ scripts/tutorial21.js
@@ -0,0 +1,6 @@
+var x = 10;
+var y = "deekshith";
+var z = true;
+var m = 10;
+var n;
+//# sourceMappingURL=tutorial21.js.map
---
+++ tutorial21.ts
@@ -0,0 +1,6 @@
+var x = 10;
+var y = "deekshith";
+var z = true;
+
+var m: number = 10;
+var n: string;
