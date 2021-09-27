77fa62bb6eeba63694ad33232387eb52752e83eb
Deekshith <76718957+deekshith786@users.noreply.github.com>
2021-09-27 10:34:25 +0530

    any type

--- scripts/tutorial21.js
+++ scripts/tutorial21.js
@@ -1,6 +1,14 @@
+// in js
 var x = 10;
 var y = "deekshith";
 var z = true;
+//in es2015 declaration
 var m = 10;
 var n;
+// typescript automatically gets the information of the varaible type by inferance
+// n = 10;   this is not possible as n is a type string 
+var info;
+info = 10;
+info = true;
+info = "hello";
 //# sourceMappingURL=tutorial21.js.map
--- tutorial21.ts
+++ tutorial21.ts
@@ -1,6 +1,17 @@
+// in js
 var x = 10;
 var y = "deekshith";
 var z = true;
 
+//in es2015 declaration
 var m: number = 10;
-var n: string;
+var n: string; 
+
+// typescript automatically gets the information of the varaible type by inferance
+
+// n = 10;   this is not possible as n is a type string 
+
+var info: any;
+info = 10;
+info = true;
+info = "hello"
