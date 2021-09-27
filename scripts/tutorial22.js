32282806b103df51766ee33fa1e4d9a4d2dee19c
Deekshith <76718957+deekshith786@users.noreply.github.com>
2021-09-27 10:38:01 +0530

    enum keyword

---
+++ scripts/tutorial22.js
@@ -0,0 +1,9 @@
+var Eyecolor;
+(function (Eyecolor) {
+    Eyecolor[Eyecolor["balck"] = 5] = "balck";
+    Eyecolor[Eyecolor["blue"] = 10] = "blue";
+    Eyecolor[Eyecolor["brown"] = 1] = "brown";
+})(Eyecolor || (Eyecolor = {}));
+var myEyecolor = Eyecolor.balck;
+console.log(myEyecolor);
+//# sourceMappingURL=tutorial22.js.map
---
+++ tutorial22.ts
@@ -0,0 +1,5 @@
+enum Eyecolor { balck = 5, blue = 10, brown = 1 }
+var myEyecolor = Eyecolor.balck;
+
+
+console.log(myEyecolor);
